ec215b831344cd1655b274c85be0dbe74f12c0d6
Yoong Shin Chow <yoongshinchow@Yoongs-MacBook-Pro.local>
2016-09-08 09:38:11 +0800

    remove lesson6_problemset1.p and changes to lesson2_problemset3.py

--- lesson2_problemset3.py
+++ lesson2_problemset3.py
@@ -48,3 +48,134 @@ print("Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me so
 print ("Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity')
 print ("Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity')
 print ("Test case 4: ", fix_machine('wsx0-=mttrhix', 't-shirt') == 't-shirt')
+
+
+
+
+#############################################################################
+# By Websten from forums
+#
+# Given your birthday and the current date, calculate your age in days.
+# Account for leap days.
+#
+# Assume that the birthday and current date are correct dates (and no
+# time travel).
+##############################################################################
+from datetime import date
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    ##
+    # Your code here.
+    ##
+    date1 = date(year1, month1, day1)
+    date2 = date(year2, month2, day2)
+    td = date2 - date1
+    return td.days
+
+# Test routine
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58),
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
+
+
+
+
+##############################################################################
+# By AnnaGajdova from forums
+# You are in the middle of a jungle.
+# Suddenly you see an animal coming to you.
+# Here is what you should do if the animal is:
+
+# zebra >> "Try to ride a zebra!"
+# cheetah >> If you are faster than a cheetah: "Run!"
+#            If you are not: "Stay calm and wait!".
+#            The speed of a cheetah is 115 km/h.
+# anything else >> "Introduce yourself!"
+
+# Define a procedure, jungle_animal,
+# that takes as input a string and a number,
+# an animal and your speed (in km/h),
+# and prints out what to do.
+##############################################################################
+def jungle_animal(animal, my_speed):
+    # YOUR CODE HERE
+    cheetah_speed = 115
+    if (animal == 'cheetah'):
+        print "Run!" if (my_speed > 115) else "Stay calm and wait!"
+    elif (animal == 'zebra'):
+        print "Try to ride a zebra!"
+    else:
+        print "Introduce yourself!"
+
+
+jungle_animal('zebra', 130)
+
+jungle_animal('cheetah', 30)
+#>>> "Stay calm and wait!"
+
+jungle_animal('gorilla', 21)
+#>>> "Introduce yourself!"
+
+
+
+
+##############################################################################
+# By Ashwath from forums
+# A leap year baby is a baby born on Feb 29, which occurs only on a leap year.
+# Define a procedure is_leap_baby that takes 3 inputs: day, month and year
+# and returns True if the date is a leap day (Feb 29 in a valid leap year)
+# and False otherwise.
+
+# A year that is a multiple of 4 is a leap year unless the year is
+# divisible by 100 but not a multiple of 400 (so, 1900 is not a leap
+# year but 2000 and 2004 are).
+##############################################################################
+def is_leap_baby(day,month,year):
+    # Write your code after this line.
+    if ((day == 29) and (month == 2)):
+        if (year % 400 == 0):
+            return True
+        elif (year % 4 == 0):
+            return True if (year % 100 <> 0) else False
+        else:
+            return False
+    else:
+        return False
+
+# The function 'output' prints one of two statements based on whether
+# the is_leap_baby function returned True or False.
+
+def output(status,name):
+    if status:
+        print "%s is one of an extremely rare species. He is a leap year baby!" % name
+    else:
+        print "There's nothing special about %s's birthday. He is not a leap year baby!" % name
+
+# Test Cases
+
+output(is_leap_baby(29, 2, 1996), 'Calvin')
+#>>>Calvin is one of an extremely rare species. He is a leap year baby!
+
+output(is_leap_baby(19, 6, 1978), 'Garfield')
+#>>>There's nothing special about Garfield's birthday. He is not a leap year baby!
+
+output(is_leap_baby(29, 2, 2000), 'Hobbes')
+#>>>Hobbes is one of an extremely rare species. He is a leap year baby!
+
+output(is_leap_baby(29, 2, 1900), 'Charlie Brown')
+#>>>There's nothing special about Charlie Brown's birthday. He is not a leap year baby!
+
+output(is_leap_baby(28, 2, 1976), 'Odie')
+#>>>There's nothing special about Odie's birthday. He is not a leap year baby!
